dd622556bc4890b9ce9dc09d5f20ff13e5500993
michalrudy9 <michallrudy9@gmail.com>
2024-03-28 23:19:23 +0100

    Added router and redux to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import HomePage from "./pages/Home";
+import TagDetailPage from "./pages/TagDetailPage";
+import store from "./store/store";
 
 const App = () => {
   const queryClient = new QueryClient();
+
+  const router = createBrowserRouter([
+    { path: "/", element: <HomePage /> },
+    { path: ":tagName", element: <TagDetailPage /> },
+  ]);
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <HomePage />
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </Provider>
   );
 };
 
