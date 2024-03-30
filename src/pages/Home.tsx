7afaa750324662971406f22341ceb6cce589cdcc
michalrudy9 <michallrudy9@gmail.com>
2024-03-30 14:50:55 +0100

    Append range result display component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,19 @@
 import { useQuery } from "@tanstack/react-query";
 import { Box } from "@mui/joy";
+import { useSelector } from "react-redux";
 
 import Header from "../components/Header";
 import { fetchTags } from "../api/homePageFunctions";
 import SelectAmountOfElements from "../components/SelectAmountOfElements";
 import ListTags from "../components/ListTags";
 import SortTags from "../components/SortTags";
+import RangeResultsDisaplay from "../components/RangeResultsDisplay";
+import { RootState } from "../store/store";
 
 const HomePage = () => {
+  const amountOfTags = useSelector<RootState, string>(
+    (state) => state.amountOfTags.amount
+  );
   const { data, isPending, isError, error, isRefetching } = useQuery({
     queryKey: ["tags"],
     queryFn: fetchTags,
@@ -24,7 +30,12 @@ const HomePage = () => {
         {isPending && <p>Loading...</p>}
         {isRefetching && <p>Applying selected changes...</p>}
         {isError && <p className="text-red-500">{error.message}</p>}
-        {!isPending && !isError && !isRefetching && <ListTags tags={data} />}
+        {!isPending && !isError && !isRefetching && (
+          <Box>
+            <ListTags tags={data} />
+            {amountOfTags !== "all" && <RangeResultsDisaplay />}
+          </Box>
+        )}
       </main>
     </>
   );
